2263989fcd21ac4af764dd29f4f4451c1bb0acfb
Tom Gilbard <tomgilbard@gmail.com>
2025-04-07 22:21:03 +0200

    ajout des la page principale et des premiers elements de css

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Liste des utilisateurs</title>
+    <title>The Sup Movie Base</title>
+    <link rel="stylesheet" href="\style\header_style.css">
+    <link rel="stylesheet" href="\style\main_style.css">
 </head>
+
 <body>
-    <h1>coucou</h1>
-    <h2>jaaaaaj</h2>
+    <header>
+        <h1>The Sup Movie Base</h1>
+    </header>
+    <div>
+        <h2>Bienvenu !</h2>
+    </div>
+
 </body>
+
 </html>
